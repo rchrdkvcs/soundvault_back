883a907f2e3a9132a432eddc3202c89b65109342
rchrdkvcs <gumimacihun@gmail.com>
2025-06-30 16:09:24 +0200

    feat: add metaFiles configuration for Swagger documentation

--- adonisrc.ts
+++ adonisrc.ts
@@ -46,7 +46,7 @@ export default defineConfig({
     () => import('@adonisjs/core/providers/vinejs_provider'),
     () => import('@adonisjs/cors/cors_provider'),
     () => import('@adonisjs/lucid/database_provider'),
-    () => import('@adonisjs/auth/auth_provider')
+    () => import('@adonisjs/auth/auth_provider'),
   ],
 
   /*
@@ -83,4 +83,11 @@ export default defineConfig({
     ],
     forceExit: false,
   },
+
+  metaFiles: [
+    {
+      pattern: 'swagger.yml',
+      reloadServer: false,
+    },
+  ],
 })
