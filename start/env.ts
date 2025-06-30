67969829acba2650d84925de4b35d132562738c1
rchrdkvcs <gumimacihun@gmail.com>
2025-06-30 15:24:21 +0200

    feat: update dependencies, enhance environment configuration, and add documentation generation command

--- start/env.ts
+++ start/env.ts
@@ -11,21 +11,28 @@
 
 import { Env } from '@adonisjs/core/env'
 
-export default await Env.create(new URL('../', import.meta.url), {
-  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
-  APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
-  LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+const isNodeAce = process.argv.some((arg) => arg.endsWith('/ace') || arg === 'ace')
 
-  /*
-  |----------------------------------------------------------
-  | Variables for configuring database connection
-  |----------------------------------------------------------
-  */
-  DB_HOST: Env.schema.string({ format: 'host' }),
-  DB_PORT: Env.schema.number(),
-  DB_USER: Env.schema.string(),
-  DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string()
-})
+export default await Env.create(
+  new URL('../', import.meta.url),
+  isNodeAce
+    ? {}
+    : {
+        NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
+        PORT: Env.schema.number(),
+        APP_KEY: Env.schema.string(),
+        HOST: Env.schema.string({ format: 'host' }),
+        LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
+
+        /*
+    |----------------------------------------------------------
+    | Variables for configuring database connection
+    |----------------------------------------------------------
+    */
+        DB_HOST: Env.schema.string({ format: 'host' }),
+        DB_PORT: Env.schema.number(),
+        DB_USER: Env.schema.string(),
+        DB_PASSWORD: Env.schema.string.optional(),
+        DB_DATABASE: Env.schema.string(),
+      }
+)
